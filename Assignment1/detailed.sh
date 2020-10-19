35f72e5f2a6f36b00decdc7688d5fcc6d3dd69dd
myousefa <yousefali.m44@gmail.com>
2020-10-18 20:33:22 -0700

    completed project + comments

--- Assignment1/detailed.sh
+++ Assignment1/detailed.sh
@@ -2,10 +2,13 @@
 
 # ---------------- DETAILED REPORT -----------------
 echo -e "Multi Genre Artists: " 
-find -mindepth 3 -maxdepth 3 -printf '%f\n' | sort | uniq -d | 
+# gets the multi genre artists
+find -mindepth 3 -maxdepth 3 -printf '%f\n' | sort | uniq -d |
+# each time it gets an artists do the following  
 while read artists; do 
     echo "  $artists"
-    find -iname "*$artists*" | cut -d/ -f 3 | sort | uniq | 
+    # gets the genres
+    find -iname "*$artists*" | cut -d/ -f 3 | sort | uniq |
     while read genres; do
         echo "      $genres"
     done
@@ -13,9 +16,11 @@ done
 echo
 
 echo -e "Multi Disk Albums: "
+# gets the multi disk artists 
 find -mindepth 5 -maxdepth 5 -iname "dis??" | cut -d / -f 4 | sort | uniq -d |
 while read artists; do
     echo "  $artists"
+    # gets the multi disk album name
     find -mindepth 5 -maxdepth 5 -iname "dis??" | grep "$artists" | cut -d / -f 5 | sort | uniq |
     while read albums; do
         echo "       $albums"
@@ -24,9 +29,18 @@ done
 echo
 
 echo -e "Possible Duplicate Albums: "
-find 
-
-
+# gets the album found in multiple places
+find -mindepth 4 -maxdepth 4 -type d | cut -d/ -f 5 | sort | uniq -d | 
+while read albums; do
+    echo "  $albums"
+    # gets the different artists and genres
+    # using sed command to parse the words to get rid of '/' and replace with ' '
+    find -mindepth 4 -maxdepth 4 -type d -iname "$albums" | cut -d/ -f 3,4 | sed 's|/| |g' |sort -k 2| 
+    while read all; do
+        echo "      $all"
+    done
+    # echo each time to add sspace between each album
+    echo
+done
 exit
-    
 # ---------------- DETAILED REPORT -----------------
--- Assignment1/warmup.sh
+++ Assignment1/warmup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # --------------------- WARMUP --------------------- 
+# Counts total song by ensuring file type is .ogg
 echo Total Tracks:  $(find . -type f | grep \.ogg | wc -l) 
 echo
-echo Total Artists: $(ls -1dN */*/* | cut -d / -f 3| sort | uniq | wc -l)
+# Counts artists by looking into the third part of list of file path and counting it
+echo Total Artists: $(ls -1dN */*/*/* | cut -d / -f 3| sort | uniq | wc -l)
 echo 
+# Finds all multi genre artists by checking in depth 3 and sort and checking for uniqueness
 echo -e Multi Genre Artists: '\n'"$(find -mindepth 3 -maxdepth 3 -printf '%f\n' | sort | uniq -d)"
 echo 
+# Locates a file that is either called disc or disk 5 files deep. If so add path and cut list
 echo -e Multi Disk Albums: '\n'"$(find -mindepth 5 -maxdepth 5 -iname "dis??" | cut -d/ -f 5 | sort | uniq -d)"
 echo
 # --------------------- WARMUP ---------------------
