71faca0feb3a23012bd875c067d418c41e3d269c
myousefa <yousefali.m44@gmail.com>
2020-10-18 18:58:43 -0700

    pushing before internet reset

--- Assignment1/warmup.sh
+++ Assignment1/warmup.sh
@@ -11,12 +11,3 @@ echo -e Multi Disk Albums: '\n'"$(find -mindepth 5 -maxdepth 5 -iname "dis??" | 
 echo
 # --------------------- WARMUP ---------------------
 
-# ---------------- DETAILED REPORT -----------------
-echo -e Multi Genre Artists: '\n'"$(find -mindepth 3 -maxdepth 3 -printf '%f\n' | sort | uniq -d)" | while read artists; do
-echo "   $artist" find -iname "*$artist*" | cut -d/ -f 2 | sort | uniq | while read genre; do
-echo "      $genre"
-done 
-done
-echo
-echo -e Multi Disk Albums: '\n'"$(find -mindepth 5 -maxdepth 5 -iname "dis??" | cut -d / -f 5 | sort | uniq -d)"
-# ---------------- DETAILED REPORT -----------------
