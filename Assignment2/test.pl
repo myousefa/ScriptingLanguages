ad2b06d6ae372da67a3fbf596c31ccb5f647b509
myousefa <yousefali.m44@gmail.com>
2020-11-01 18:48:39 -0800

    still working on perl sol

--- Assignment2/test.pl
+++ Assignment2/test.pl
@@ -1,43 +1,54 @@
 #!/usr/bin/perl -l
 use warnings;
 use strict;
+use Data::Dumper;
 
 my $filename = 'input.txt';
 
-open(FH, '<', $filename) or die $!;
-my %music_db = ();
-while(<FH>){
-    my @line_data = split('/',$_);
-    
-
-    my $artist = $line_data[2];
-    my $album = $line_data[3];
-    my $track = $line_data[-1];
-
-    my %artist_projects = ();
-    our @album_tracks = ();
-    if(!exists $music_db{$artist}){
-        # checking the artist is NOT in the Hash
-        push(@album_tracks,$track);
-        $music_db{$artist}{$album} = @album_tracks;
-        #$music_db{$artist} = {$album=>5};
-
-        #$music_db{$artist} => $artist_projects;
-        #push(@album_tracks,$track)
-        #%music_db{$artist} = ($album => @album_tracks);
-        #%music_db{$artist}{$album} => @album_tracks;
-    }
-    # elsif(exists $music_db{$artist}){
-    #     # checking the artist IS in the Hash
-    #     # add the additional album
-    #     $music_db{$artist} = ($album =>)
-    # }
-
-    # foreach my $i (@line_data)  
-    # {
+sub read_file {
+    open(FH, '<', $filename) or die $!;
+    my %music_db = ();
+    my @album_tracks = ();
+    while(<FH>){
+        my @line_data = split('/',$_);
+        my $artist = $line_data[2];
+        my $album = $line_data[3];
+        my $track = $line_data[-1];
+        my %artist_projects = ();
         
-    #     print "$i --> "; 
-    # }
+        if(!exists $music_db{$artist}){
+            # checking the artist is NOT in the Hash
+            push(@album_tracks,$track);
+            $music_db{$artist}{$album} = [@album_tracks];
+        }
+        elsif(exists $music_db{$artist} && exists $music_db{$artist}{$album}){
+            # checking the artist IS in the Hash
+            # add the current track to the existing album
+            push(@album_tracks,$track);
+            $music_db{$artist}{$album} = [ @album_tracks ];
+        }
+        elsif($music_db{$artist} && !exists $music_db{$artist}{$album}){
+            # checking the artist IS in the Hash
+            # add the current track to the new album
+            @album_tracks = ();
+            push(@album_tracks,$track);
+            $music_db{$artist}{$album} =  [@album_tracks];
+        }
+    }
+    close(FH);
+    return %music_db
 }
 
-close(FH);
+
+
+my %music_db = read_file();
+#print Dumper(\%music_db);
+# #print(%music_db);
+foreach my $artist (keys %music_db){
+    while ((my $album, my @album_details) = each %{$music_db{$artist}} ){
+        print($artist,"--> ",@album_details," --> ", @album_details[0]);
+        foreach my $track ( @album_details ){
+            print($track);
+        }
+    }
+}
