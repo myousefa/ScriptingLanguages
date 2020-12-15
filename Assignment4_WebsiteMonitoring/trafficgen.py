f9a092efb4e13f15db9dd8dcf7d2c768f7db1a29
myousefa <yousefali.m44@gmail.com>
2020-12-14 22:51:00 -0800

    trafficgen updates

--- Assignment4_WebsiteMonitoring/trafficgen.py
+++ Assignment4_WebsiteMonitoring/trafficgen.py
@@ -3,6 +3,35 @@ import argparse
 import random
 import time 
 import math
+import sys
+import urllib2
 
-# The purpose of this program is generate traffic to our server
+default_port = 8080
 
+# Process cmd line args
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', type = str)
+parser.add_argument('--rps', type = float)
+parser.add_argument('--jitter', type = float)
+args = parser.parse_args()
+
+# Put them into usable fields
+url = args.url
+rps = args.rps
+jitter = args.jitter
+
+# Ensurinng jitter is in appropriate range of .1-1.0
+if jitter < 0.1: 
+    jitter = 0.1
+elif jitter > 1:
+    jitter = 1
+
+lower_bound = rps*(1.0-jitter)
+upper_bound = rps*(1.0+jitter)
+
+print(jitter)
+# ---------------------------------- Initial set up done ----------------------------------
+
+# Generate Requests to url
+while True:
+    ji
