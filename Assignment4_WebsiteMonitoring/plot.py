c1981c9e3499fd86f1b3b251c07c4378cf44a586
myousefa <yousefali.m44@gmail.com>
2020-12-18 21:31:38 -0800

    added graph viz

---
+++ Assignment4_WebsiteMonitoring/plot.py
@@ -0,0 +1,68 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import sys
+import os
+
+
+def grab_data():
+    data = []
+    data_200 = []
+    data_404 = []
+    data_500 = []
+    rps = 0
+    with open('data.tsv', 'r', buffering = 1) as tsvfile:
+        for line in tsvfile:
+            line = line.split()
+            time = line[0]
+            data_500.append(int(line[1]))
+            data_200.append(int(line[2]))
+            data_404.append(int(line[3]))
+    # Remove first record
+    data_200 = data_200[1:]
+    data_404 = data_404[1:]
+    data_500 = data_500[1:]
+
+    data.append(data_200)
+    data.append(data_404)
+    data.append(data_500)
+
+    # Set up for RPM
+    rpm_200 = np.zeros( (len(data[0])//6),) 
+    rpm_404 = np.zeros( (len(data[0])//6),) 
+    rpm_500 = np.zeros( (len(data[0])//6),)
+    rpm_data = np.array([rpm_200,rpm_404,rpm_500])
+
+    for rt,req_type in enumerate(data):
+        record_count = 0
+        min_total = 0
+        for i in range(len(req_type)):
+            if(record_count == 6):
+                #rpm_data[rt] = np.append(rpm_data[rt],min_total)
+                #rpm_data[rt].append(min_total)
+                #print()
+                rpm_data[rt,int(float(i/60)*10)] = min_total
+                min_total = 0
+                record_count = 0
+                min_total += req_type[i]
+                record_count += 1
+            else:
+                min_total += req_type[i]
+                record_count += 1
+    
+    rpm_data[0] = np.array(rpm_data[0])
+    rpm_data[1] = np.array(rpm_data[1])
+    rpm_data[2] = np.array(rpm_data[2])
+
+    rps_data = rpm_data[:] // 60
+
+    fig,ax = plt.subplots(1)
+
+    plt.plot(rps_data[0],color='r')
+    plt.plot(rps_data[1],color='g')
+    plt.plot(rps_data[2],color='b')
+
+    plt.savefig("graph.png")
+    
+grab_data()
